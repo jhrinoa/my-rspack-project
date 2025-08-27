8976c0c6ca270adca4b4b3b635be872d36483d8a
Jonathan Lee <jon.lee@samsung.com>
2025-08-26 22:52:06 -0700

    initial setup

---
+++ rspack.config.mjs
@@ -0,0 +1,87 @@
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from '@rspack/cli';
+import { rspack } from '@rspack/core';
+import { ReactRefreshRspackPlugin } from '@rspack/plugin-react-refresh';
+import { ModuleFederationPlugin } from '@module-federation/enhanced/rspack';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const isDev = process.env.NODE_ENV === 'development';
+
+// Target browsers, see: https://github.com/browserslist/browserslist
+const targets = ['last 2 versions', '> 0.2%', 'not dead', 'Firefox ESR'];
+
+export default defineConfig({
+  context: __dirname,
+  entry: {
+    main: './src/main.jsx',
+  },
+  resolve: {
+    extensions: ['...', '.ts', '.tsx', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        type: 'asset',
+      },
+      {
+        test: /\.(jsx?|tsx?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'builtin:swc-loader',
+            options: {
+              jsc: {
+                parser: {
+                  syntax: 'typescript',
+                  tsx: true,
+                },
+                transform: {
+                  react: {
+                    runtime: 'automatic',
+                    development: isDev,
+                    refresh: isDev,
+                  },
+                },
+                experimental: {
+                  plugins: [['swc-plugin-coverage-instrument', {}]],
+                },
+              },
+
+              env: { targets },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    // new ModuleFederationPlugin({
+    //   name: 'host',
+    //   remotes: {
+    //     provider:
+    //       'rslib_provider@https://unpkg.com/module-federation-rslib-provider@latest/dist/mf/mf-manifest.json',
+    //   },
+    //   shared: {
+    //     react: { singleton: true, eager: true },
+    //     'react-dom': { singleton: true, eager: true },
+    //   },
+    // }),
+    new rspack.HtmlRspackPlugin({
+      template: './index.html',
+    }),
+    isDev ? new ReactRefreshRspackPlugin() : null,
+  ].filter(Boolean),
+  optimization: {
+    minimizer: [
+      new rspack.SwcJsMinimizerRspackPlugin(),
+      new rspack.LightningCssMinimizerRspackPlugin({
+        minimizerOptions: { targets },
+      }),
+    ],
+  },
+  experiments: {
+    css: true,
+  },
+});
