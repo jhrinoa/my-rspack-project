9b42d673c815ff59bfd8b6cf7a4696dfd989cfb1
Jonathan Lee <jon.lee@samsung.com>
2025-08-26 22:59:35 -0700

    simplify demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
 
 	return (
 		<div className="App">
@@ -13,17 +11,11 @@ function App() {
 				</a>
 			</div>
 			<h1>Rspack + React</h1>
-			<div className="card">
-				<button type="button" onClick={() => setCount(count => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
+			<div>
+				{Object.keys(window.__coverage__).map((key) => {
+					return <div>Path Key: [{key}]</div>
+				})}
 			</div>
-			<p className="read-the-docs">
-				Click on the Rspack and React logos to learn more
-			</p>
 		</div>
 	);
 }
